26de1baea7bad74b65ccba602a3c042063569541
Денис Божков <stu2201261006@uni-plovdiv.bg>
2024-03-20 18:15:52 +0200

    Created data view and create form

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+	$id=isset($_GET['id'])??-1;
+	try
+	{
+		$link=mysqli_connect("localhost","root","","testPHPSite");
+		$result=mysqli_query($link,"delete from users where id=$id");
+		if(mysqli_affected_rows($link)==0)
+			echo "Element with id $id does not exist!";
+		else header("Location:index.php");
+	}
+	catch(Exception $e)
+	{
+		print_r($e);
+	}
+?>
--- index.php
+++ index.php
@@ -1,20 +1,43 @@
+<h1>Users</h1>
+<a href="create.html">Create new user</a>
+<table border="1">
+<tr>
+	<th>Id</th>
+	<th>First name</th>
+	<th>Last name</th>
+	<th>Delete</th>
+</tr>
 <?php
 	function prepair_db($link)
 	{
 		mysqli_query($link,"create database testPHPSite");
 		mysqli_select_db($link,"testPHPSite");
-		$sql="create table users(id int auto_increment primary key,firstname varchar(50) not null,lastname varchar(50) not null)";
+		$sql="create table users(
+				id int auto_increment primary key,
+				firstname varchar(50) not null,
+				lastname varchar(50) not null)";
 		mysqli_query($link,$sql);
 	}
+	
 	$link=mysqli_connect("localhost","root","");
 	try
 	{
 		mysqli_select_db($link,"testPHPSite");
-		echo "Existed";
 	}
 	catch(Exception)
 	{
 		prepair_db($link);
-		echo "Created";
+	}
+	
+	$result=mysqli_query($link,"select * from users");
+	while($data=mysqli_fetch_array($result))
+	{
+		echo "<tr>";
+		echo "<td>".$data['id']."</td>";
+		echo "<td>".$data['firstname']."</td>";
+		echo "<td>".$data['lastname']."</td>";
+		echo '<td><a href="delete.php?id='.$data["id"].'">Delete</a></td>';
+		echo "</tr>";
 	}
 ?>
+</table>
